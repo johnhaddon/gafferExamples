40d596f8e10ee932fe36bb2126fbcd410008f9f1
John Haddon <thehaddonyoof@gmail.com>
2018-04-19 16:55:48 +0100

    Add userDefaults example

---
+++ startup/gui/userDefaults.py
@@ -0,0 +1,7 @@
+import imath
+import GafferScene
+
+# User defaults are applied automatically whenever a node is created by the user through the UI.
+
+Gaffer.Metadata.registerValue( GafferScene.Sphere, "radius", "userDefault", 100.0 )
+Gaffer.Metadata.registerValue( GafferScene.StandardOptions, "options.renderResolution.value", "userDefault", imath.V2i( 2048, 1556 ) )
